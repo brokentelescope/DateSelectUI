6f087b1cbe5b19c3acc62aaab32eb9cace80840a
Owen Chen <owenchenyp@gmail.com>
2024-05-31 09:18:15 -0400

    improved draggin mechanism

--- candle.js
+++ candle.js
@@ -1,8 +1,5 @@
-// Click and Drag an object
-// Daniel Shiffman <http://www.shiffman.net>
-
-class Draggable {
-    constructor(x, y, w, h) {
+class Candle {
+  constructor(x, y, w, h, img) {
       this.dragging = false; // Is the object being dragged?
       this.rollover = false; // Is the mouse over the ellipse?
       this.x = x;
@@ -11,50 +8,51 @@ class Draggable {
       this.h = h;
       this.offsetX = 0;
       this.offsetY = 0;
-    }
+      this.img = img;
+  }
   
-    over() {
+  over() {  
       // Is mouse over object
       if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
-        this.rollover = true;
+          this.rollover = true;
       } else {
-        this.rollover = false;
+          this.rollover = false;
       }
-    }
-  
-    update() {
+  }
+
+  update() {
       // Adjust location if being dragged
       if (this.dragging) {
-        this.x = mouseX + this.offsetX;
-        this.y = mouseY + this.offsetY;
+          this.x = mouseX + this.offsetX;
+          this.y = mouseY + this.offsetY;
       }
-    }
-  
-    show() {
+  }
+
+  show() {
       stroke(0);
       // Different fill based on state
       if (this.dragging) {
-        fill(50);
+          fill(50);
       } else if (this.rollover) {
-        fill(100);
+          fill(100);
       } else {
-        fill(175, 200);
+          fill(175, 200);
       }
-      rect(this.x, this.y, this.w, this.h);
-    }
-  
-    pressed() {
+      image(this.img, this.x, this.y, this.w, this.h);
+  }
+
+  pressed() {
       // Did I click on the rectangle?
       if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
-        this.dragging = true;
-        // If so, keep track of relative location of click to corner of rectangle
-        this.offsetX = this.x - mouseX;
-        this.offsetY = this.y - mouseY;
+          this.dragging = true;
+          // If so, keep track of relative location of click to corner of rectangle
+          this.offsetX = this.x - mouseX;
+          this.offsetY = this.y - mouseY;
       }
-    }
-  
-    released() {
+  }
+
+  released() {
       // Quit dragging
       this.dragging = false;
-    }
   }
+}
